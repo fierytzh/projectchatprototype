3ceb7e0412f469df53825fce333a197d2744279e
fierytzh <fierytzh@gmail.com>
2018-05-03 14:30:49 +0800

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=tempbotchat-Web-Deploy.pubxml -p:Password=NLEcpZYS2XTuHKze2QMjGndY6r6SPQlkNGdtefLmzRQqxnA53tuAhsTN79Q2
+
